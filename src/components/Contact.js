012ac033aa7e8af1a73b6f161544526521d580e6
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-01-12 17:46:49 -0500

    front end edits

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -39,8 +39,7 @@ export default function Contact() {
                 ADDRESS
               </h2>
               <p className="mt-1">
-                97 Warren St. <br />
-                New York, NY 10007
+                Richmond, Va <br />
               </p>
             </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
@@ -48,12 +47,12 @@ export default function Contact() {
                 EMAIL
               </h2>
               <a className="text-indigo-400 leading-relaxed">
-                reedbarger@email.com
+                emk2473@email.com
               </a>
-              <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+              {/* <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
-              <p className="leading-relaxed">123-456-7890</p>
+              <p className="leading-relaxed">804-937-3742</p> */}
             </div>
           </div>
         </div>
