52732a6253a8dfb7feef611c359e3eb4925b8cda
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-01-12 20:21:30 -0500

    new gifs for projects

--- src/data.js
+++ src/data.js
@@ -1,18 +1,18 @@
 export const projects = [
-  {
-    title: "Password Generator",
-    subtitle: "Learning JavaScript Functions",
-    description:
-      "A Random Password Generator.",
-    image: "./project-1.gif",
-    link: "https://emk2473.github.io/password-generator/",
-  },
+  // {
+  //   title: "Password Generator",
+  //   subtitle: "Learning JavaScript Functions",
+  //   description:
+  //     "A Random Password Generator.",
+  //   image: "./project-1.gif",
+  //   link: "https://emk2473.github.io/password-generator/",
+  // },
   {
     title: "JavaScript Quiz",
     subtitle: "JavaScript Quiz",
     description:
       "A quix designed to test your knowledge of JavaScript",
-    image: "./project-2.gif",
+    image: "./jsquiz.gif",
     link: "https://emk2473.github.io/javascript-quiz/",
   },
   {
@@ -20,7 +20,7 @@ export const projects = [
     subtitle: "Sundial Daily Planner",
     description:
       "A Daily 9-5 Planner to help manage your time effectively.",
-    image: "./project-3.gif",
+    image: "./planner.gif",
     link: "https://emk2473.github.io/daily-planner/",
   },
   {
@@ -28,41 +28,41 @@ export const projects = [
     subtitle: "5-Day Weather Forecast",
     description:
       "A 5-Day Weather Forecast Application.",
-    image: "./project-4.gif",
+    image: "./5dayGIF.gif",
     link: "https://emk2473.github.io/weather-forecast/",
   },
-  {
-    title: "Inventory Management System",
-    subtitle: "IMS",
-    description:
-      "An inventory management database for companies to manage their inventory through users providing live an up-to-date inventory information.",
-    image: "./project-4.gif",
-    link: "https://inv-man-system-b9da5a82b52b.herokuapp.com/login",
-  },
+  // {
+  //   title: "Inventory Management System",
+  //   subtitle: "IMS",
+  //   description:
+  //     "An inventory management database for companies to manage their inventory through users providing live an up-to-date inventory information.",
+  //   image: "./project-4.gif",
+  //   link: "https://inv-man-system-b9da5a82b52b.herokuapp.com/login",
+  // },
   {
     title: "Note Taker",
     subtitle: "Take notes with Express",
     description:
       "An Application for writing and saving notes using a server.",
-    image: "./project-4.gif",
+    image: "./expNoteGIF.gif",
     link: "https://exp-note-taker-app-7afdb91c4244.herokuapp.com/",
   },
-  {
-    title: "J.A.T.E.",
-    subtitle: "Just Another Text Editor",
-    description:
-      "A text editor that runs in the browser. Create notes or code snippets with or without an internet connection so you can reliably retrieve them for later use!",
-    image: "./project-4.gif",
-    link: "https://j-a-t-e-867088540b22.herokuapp.com/",
-  },
-  {
-    title: "Calliope's Commonplace",
-    subtitle: "An App designed to embody Calliope",
-    description:
-      "A Group Project designed to introduce API's and CSS Frameworks.",
-    image: "./project-4.gif",
-    link: "https://emk2473.github.io/calliope-commonplace/",
-  },
+  // {
+  //   title: "J.A.T.E.",
+  //   subtitle: "Just Another Text Editor",
+  //   description:
+  //     "A text editor that runs in the browser. Create notes or code snippets with or without an internet connection so you can reliably retrieve them for later use!",
+  //   image: "./project-4.gif",
+  //   link: "https://j-a-t-e-867088540b22.herokuapp.com/",
+  // },
+  // {
+  //   title: "Calliope's Commonplace",
+  //   subtitle: "An App designed to embody Calliope",
+  //   description:
+  //     "A Group Project designed to introduce API's and CSS Frameworks.",
+  //   image: "./callCommGIF.gif",
+  //   link: "https://emk2473.github.io/calliope-commonplace/",
+  // },
 ];
 
 export const testimonials = [
