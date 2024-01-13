5a07a3abe2962643032c032cc4638a83b9b14245
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-01-13 12:40:13 -0500

    bio edits

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="title-font font-medium text-white mb-4 md:mb-0">
           <a href="#about" className="ml-3 text-xl">
-            Reed Barger
+            Eric Keeton
           </a>
         </a>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
@@ -20,6 +20,9 @@ export default function Navbar() {
           </a>
           <a href="#testimonials" className="mr-5 hover:text-white">
             Testimonials
+          </a>
+          <a href="#working" className="mr-5 hover:text-white">
+            Currently Working On
           </a>
         </nav>
         <a
